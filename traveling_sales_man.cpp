e13605225b3a5a29701a4e425fe03a0955d2dd9c
NickWarren317 <yoitsbiggiecheese@gmail.com>
2024-01-29 16:23:15 -0600

    brute force added

--- traveling_sales_man.cpp
+++ traveling_sales_man.cpp
@@ -23,11 +23,14 @@ class city_map{
     int path_length;
     int start_index;
 
+    int brute_min_path;
+
 
     vector<vector<int>> map;
     vector<node> node_list;
     vector<node> ideal_path;
 
+    vector<int> brute_ideal_path;
 
     public:
     city_map(vector<vector<int>> cities){
@@ -114,6 +117,31 @@ class city_map{
 
     }
 
+    void brute_force(){
+        printf("H");
+        brute_force_helper(-1,0,0,0);
+        printf("Path length of: %d", brute_min_path);
+    }
+
+    void brute_force_helper(int length, int nodes_in_path, int last_node, int first_node){
+        if(length == -1){ //first call
+            for(int i = 0; i < num_cities; i++){
+                if(map[i][last_node] != 0 && i != 0) brute_force_helper(0,1,i,i); //each is a start node.
+            }
+        } else if(nodes_in_path < num_cities){ //recurse on all adjacent nodes
+            for(int i = 0; i < num_cities; i++){
+                if(map[i][last_node] != 0 && i != last_node){
+                    brute_force_helper(length + map[last_node][i], nodes_in_path++, i, first_node);
+                }
+            }
+        } else if(nodes_in_path == num_cities){ //when all nodes are in path, check length against current min
+            length += map[last_node][first_node];
+            if(length < brute_min_path){
+                brute_min_path = length;
+            }
+        }
+    }
+
     void printMatrix(){
         printf("     ");
         for(int i = 0; i < num_cities; i++){
@@ -142,13 +170,48 @@ class city_map{
         printf("Path length of: %d", path_length);
     }
 
+    void mid_path_search(){
+        int start, end;
+        int max = 0;
+        start = end = num_cities + 5; //both are inpossible
+        for(int y = 0; y < num_cities; y++){
+            for(int x = y; x < num_cities; x++){
+                if(map[y][x] > max){
+                    max = map[y][x];
+                    start = x;
+                    end = y;
+                }
+            }                                           
+                                                            
+        } 
+
+        printf("%d and %d are %d away!", start, end, max);                                                                                                                          
+    }                                                               
 
 };
 
+
 //start at node k, find the farthest node from k, make a path there using half the remaining nodes, make a path back with the remaining.
+/*
+    find max distance between cities, use as 0 and n;
+        lg(n)
 
+    for nodes adjactent to n:
+        get max
+    farthest node = max
+        n^2
 
-vector<vector<int>> random_map_generator(int size){
+    find shortest path from start to max,
+    find shortest path from max to start.
+
+    traverse both paths:
+        at n cities from start, try to swap the distances to see if the path can be optimized.
+
+*/
+
+//maybe a merge for finding the path? 0->n->0  0->n/2->n->3n/2->0 ...
+
+vector<vector<int>> random_map_generator(int size, int max_distance){
     vector<int> new_row;
     vector<vector<int>> ans;
 
@@ -156,7 +219,7 @@ vector<vector<int>> random_map_generator(int size){
         if(i == 0){
             new_row.push_back(0);
         } else {
-            new_row.push_back(rand() % 500 + 1);
+            new_row.push_back(rand() % max_distance + 1);
         }
         
     }
@@ -171,7 +234,7 @@ vector<vector<int>> random_map_generator(int size){
             } else if(x == y){
                 new_row.push_back(0);
             } else {
-                new_row.push_back(rand() % 500 + 1);
+                new_row.push_back(rand() % max_distance + 1);
             } 
         }
         ans.push_back(new_row);
@@ -182,8 +245,7 @@ vector<vector<int>> random_map_generator(int size){
 }
 
 
-int main(int argc, char **argv){
-
+int main(int argc, char ** argv){
     srand(time(NULL));
     vector<vector<int>> input = {{0,  3,  9,  5,  14, 32, 13},
                                  {3,  0,  13, 18, 5,  31, 6},
@@ -197,8 +259,13 @@ int main(int argc, char **argv){
                                    { 10, 0, 35, 25 },
                                    { 15, 35, 0, 30 },
                                    { 20, 25, 30, 0 } };
-    city_map map = city_map(random_map_generator(30));
-    map.solve_TSP();
-    map.print_path_and_length(); 
+    printf("%d %d\n", atoi(argv[1]), atoi(argv[2]));
+    city_map map = city_map(random_map_generator(atoi(argv[1]), atoi(argv[2])));
+    //city_map map = city_map(input);
+    map.printMatrix();
+    map.brute_force();
+    //map.solve_TSP();
+    //map.print_path_and_length(); 
+    //map.mid_path_search();
     return 0;
 }
